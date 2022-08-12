86fe67b3313669db05bb87d8006d1d0525a7a2f5
Diego Miranda <diegommir@gmail.com>
2022-08-12 19:13:41 +0000

    no-cors and api uri prefix

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,20 @@
 import express from 'express'
 import bodyParser from 'body-parser'
+import cors from 'cors'
 import patrimonioRouter from './routes/PatrimonioRouter'
 import localRouter from './routes/LocalRouter'
 
 const app = express()
+const router = express.Router()
 
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+app.use(cors())
 
-app.use('/patrimonio', patrimonioRouter)
-app.use('/local', localRouter)
+router.use('/patrimonio', patrimonioRouter)
+router.use('/local', localRouter)
+
+app.use('/api/v1', router)
 
 //Default 404
 app.use(function (req, res) {
